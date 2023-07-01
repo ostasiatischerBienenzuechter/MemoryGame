cdbc28b20e8952628fee94275b2be68e5cf8db7b
Sebastian <SebastianJanowski11@web.de>
2023-07-01 17:55:30 +0200

    bug fixes

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,6 +1,6 @@
 const Score = ({ turns }) => {
   return (
-    <div className="text-violet-900 text-3xl bg-gradient-to-r from-purple-200 to-purple-100 px-6 py-1 mb-5 rounded-full">
+    <div className="text-violet-900 text-3xl bg-gradient-to-r from-purple-200 to-purple-100 px-6 py-1 mb-5 mt-5 rounded-full">
       Turns: {turns}
     </div>
   );
